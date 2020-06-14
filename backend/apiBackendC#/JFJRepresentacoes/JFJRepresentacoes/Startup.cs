77c87d9e6c04854adbdcdcfbfcc461c456008fb4
EduardoValoisFranz <38699565+eduardovaloisfranz@users.noreply.github.com>
2020-06-14 15:45:03 -0300

    usando sql hospedado freemysql

--- backend/apiBackendC#/JFJRepresentacoes/JFJRepresentacoes/Startup.cs
+++ backend/apiBackendC#/JFJRepresentacoes/JFJRepresentacoes/Startup.cs
@@ -30,7 +30,7 @@ namespace JFJRepresentacoes
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            var connection = Configuration["ConexaoMySql:MySqlConnectionString"];
+            var connection = Configuration["ConexaoMySql:FreeMySqlHosting"];
             services.AddDbContext<RepresentacaoContexto>(options =>
             {
                 options.UseMySql(connection);
